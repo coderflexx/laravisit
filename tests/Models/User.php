4dae4074487d460ee09c333487fa191285896522
ousid <ousid@users.noreply.github.com>
2022-02-09 22:27:51 +0000

    Fix styling

--- tests/Models/User.php
+++ tests/Models/User.php
@@ -5,7 +5,6 @@ namespace Coderflex\Laravisit\Tests\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-
 class User extends Authenticatable
 {
     use HasFactory;
