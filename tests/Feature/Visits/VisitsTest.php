d0cdf4ebb5977fd85bb4660588342d13804ae302
Oussama <imo.ossama1@gmail.com>
2022-08-14 22:29:12 +0100

    Fix Typo

--- tests/Feature/Visits/VisitsTest.php
+++ tests/Feature/Visits/VisitsTest.php
@@ -220,7 +220,7 @@ it('creates visits after a non-carbon custom time frame', function () {
     expect($post->visits->first()->count())->toBe(1);
 });
 
-it('does note create visits for crawler', function () {
+it('does not create visits for crawler', function () {
     $post = Post::factory()->create();
     request()->headers->set('User-Agent', 'Googlebot');
     $post->visit();
